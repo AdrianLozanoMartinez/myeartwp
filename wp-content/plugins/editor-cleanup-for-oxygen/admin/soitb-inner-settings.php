ad3e0cb5141ebffb6ac8669f9644ebcfb4744f8d
Marina Madrid García <74628083+MarinaMadrid@users.noreply.github.com>
2024-06-04 10:32:06 +0200

    trabajo de ayer

--- wp-content/plugins/editor-cleanup-for-oxygen/admin/soitb-inner-settings.php
+++
@@ -1,6 +0,0 @@
-<?php
-defined( 'FDP_SOITB_PLUGIN_DIR' ) || exit;
-
-$page = 'inner';
-$title =  esc_attr__( 'Oxygen inner editor cleanup.','editor-cleanup-for-oxygen' );
-require_once FDP_SOITB_PLUGIN_DIR.'/admin/soitb-settings.php';
--- wp-content/plugins/editor-cleanup-for-oxygen/admin/soitb-loading-settings.php
+++
@@ -1,6 +0,0 @@
-<?php
-defined( 'FDP_SOITB_PLUGIN_DIR' ) || exit;
-
-$page = 'loading';
-$title =  esc_attr__( 'Oxygen editor loading cleanup.','editor-cleanup-for-oxygen' );
-require_once FDP_SOITB_PLUGIN_DIR.'/admin/soitb-settings.php';
